fabde436d9b1f6ad101145ebec31da3a528570a4
Ammar <muhammad2000ammar@gmail.com>
2024-07-28 20:16:10 +0200

    forward pass function, addition of noise given the original image, noise, and timestep t

---
+++ noise_scheduler.py
@@ -0,0 +1,30 @@
+import torch
+
+
+class LinearNoiseScheduler:
+    def __init__(self, num_timesteps, beta_start, beta_end):
+        self.num_timesteps = num_timesteps
+        self.beta_start = beta_start
+        self.beta_end = beta_end
+
+        # create a 1-D tensor whose values are evenly spaced from start to end
+        self.betas = torch.linspace(beta_start, beta_end, num_timesteps)
+        self.alphas = 1. - self.betas
+        # returns cummulative product of elements of input (multiplies all elements of input together)
+        self.alpha_cum_prod = torch.cumprod(self.alphas, dim=0)
+        self.sqrt_alpha_cum_prod = torch.sqrt(self.alpha_cum_prod)
+        self.sqrt_one_minus_alpha_cum_prod = torch.sqrt(1. - self.alpha_cum_prod)
+
+
+    def add_noise(self, original, noise, t):
+        original_shape = original.shape
+        batch_size = original_shape[0]
+
+        sqrt_alpha_cum_prod = self.sqrt_alpha_cum_prod[t].reshape(batch_size)
+        sqrt_one_minus_alpha_cum_prod = self.sqrt_one_minus_alpha_cum_prod[t].reshape(batch_size)
+
+        for _ in range(len(original_shape)-1):
+            sqrt_alpha_cum_prod = sqrt_alpha_cum_prod.unsqueeze(-1)
+            sqrt_one_minus_alpha_cum_prod = sqrt_one_minus_alpha_cum_prod.unsqueeze(-1)
+
+        return sqrt_alpha_cum_prod*original + sqrt_one_minus_alpha_cum_prod*noise
