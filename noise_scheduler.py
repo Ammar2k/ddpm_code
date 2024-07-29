bdc114c4651d3bcaece9aca9b972234788023386
Ammar <muhammad2000ammar@gmail.com>
2024-07-29 15:49:59 +0200

    completed reverse sampling process

--- noise_scheduler.py
+++ noise_scheduler.py
@@ -10,12 +10,13 @@ class LinearNoiseScheduler:
         # create a 1-D tensor whose values are evenly spaced from start to end
         self.betas = torch.linspace(beta_start, beta_end, num_timesteps)
         self.alphas = 1. - self.betas
-        # returns cummulative product of elements of input (multiplies all elements of input together)
+        # returns cummulative product of elements of input (cumprod[a,b,c] = [a, a*b, a*b*c])
         self.alpha_cum_prod = torch.cumprod(self.alphas, dim=0)
         self.sqrt_alpha_cum_prod = torch.sqrt(self.alpha_cum_prod)
         self.sqrt_one_minus_alpha_cum_prod = torch.sqrt(1. - self.alpha_cum_prod)
 
 
+    # forward process of adding noise
     def add_noise(self, original, noise, t):
         original_shape = original.shape
         batch_size = original_shape[0]
@@ -28,3 +29,21 @@ class LinearNoiseScheduler:
             sqrt_one_minus_alpha_cum_prod = sqrt_one_minus_alpha_cum_prod.unsqueeze(-1)
 
         return sqrt_alpha_cum_prod*original + sqrt_one_minus_alpha_cum_prod*noise
+
+
+    # reverse sampling process
+    def sample_prev_timestep(self, xt, noise_pred, t):
+        x0 = (xt - self.sqrt_one_minus_alpha_cum_prod[t] * noise_pred) / self.sqrt_alpha_cum_prod[t]
+        x0 = torch.clamp(x0, -1., 1.)
+
+        mean = xt - ((self.betas[t] * noise_pred) / self.sqrt_one_minus_alpha_cum_prod[t])
+        mean = mean / torch.sqrt(self.alphas[t])
+
+        if t == 0:
+            return mean
+        else:
+            variance = (1 - self.alpha_cum_prod[t - 1]) / (1 - self.alpha_cum_prod[t])
+            variance = variance * self.betas[t]
+            sigma = variance ** 0.5
+            z = torch.randn(xt.shape).to(xt.device)
+            return mean + sigma*z, x0
