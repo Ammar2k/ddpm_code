db05bfbdd7802e64e75c3a0549d344fddfa747b2
Ammar <muhammad2000ammar@gmail.com>
2024-07-30 23:34:07 +0200

    added forward method for downblock class

--- unet.py
+++ unet.py
@@ -35,3 +35,24 @@ class DownBlock(nn.Module):
         self.residual_input_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
         self.down_sample_conv = nn.Conv2d(out_channels, out_channels, kernel_size=4,
                                         stride=2, padding=1) if self.down_sample else nn.Identity()
+
+    def forward(self, x, t_emb):
+        out = x
+        # Resnet block
+        resnet_input = out
+        out = self.resnet_conv_first(out)
+        out = out + self.t_emb_layers(t_emb)[:, :, None, None]
+        out = self.resnet_conv_second(out)
+        out = out + self.residual_input_conv(resnet_input)
+
+        # Attention block
+        batch_size, channels, h, w = out.shape
+        in_attn = out.reshape(batch_size, channels, h*w)
+        in_attn = self.attention_norm(in_attn)
+        in_attn = in_attn.transpose(1, 2)
+        out_attn, _ = self.attention(in_attn, in_attn, in_attn)
+        out_attn = out_attn.transpose(1, 2).reshape(batch_size, channels, h, w)
+        out = out + out_attn
+
+        out = self.down_sample_conv(out)
+        return out
